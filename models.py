9d393bd0c6e3629cae8157e2b40020414e261175
Fauzan Emmerling <erich@emfeld.com>
2013-02-20 11:57:20 +0700

    Standardize main and models

--- models.py
+++ models.py
@@ -5,17 +5,3 @@
 # this is due to performance considerations  #
 ##############################################
 from google.appengine.ext import ndb
-
-class User(ndb.Model):
-	user_id = ndb.StringProperty()
-	username = ndb.StringProperty()
-	firstname = ndb.StringProperty()
-	lastname = ndb.StringProperty()
-
-	# data transfer object to form JSON
-	def dto(self):
-		return dict(
-				user_id = self.user_id,
-				username = self.username,
-				firstname = self.firstname,
-				lastname = self.lastname)
